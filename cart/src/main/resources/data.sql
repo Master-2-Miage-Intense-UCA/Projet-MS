16118e48ca494723685fde0c2baceec480375e89
PaulBurel <paulburel400@gmail.com>
2023-03-14 11:45:16 +0100

    ajout datas SQL cart

---
+++ cart/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO CART(id) values
+                         (1)
+
+INSERT INTO CARTITEM(id, productID, quantity) values
+                                                  (1, 2, 3),
+                                                  (1, 1, 1),
+                                                  (1, 3, 2),
